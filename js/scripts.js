2151406da1fdf671e09200aa968ac543a46800c2
Mel Yasuda and Vera Weikel <vera@>
2018-08-09 13:32:15 -0700

    It will update to reflect gender, interests and hobbies resulting in pairing for all file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,31 @@
 $(document).ready(function() {
-  var height = parseInt(prompt("How tall are you?"));
 
-if (height >= 60) {
-  $("#adultAllowableRides").addClass("highlight").show();
-  $("#childrenAllowableRides").show();
-} else {
-    $("#childrenAllowableRides").addClass("highlight").show();
-    $("#adultAllowableRides").show();
+
+if (gender === optionA) {
+  $("#gender").show("Please make sure all selections have been made.");
+
+
+  if (#gender === optionB) {
+    $('#drinks').show();
+  } else if (age === optionA) {
+    alert("Now don't go crazy!");
+    $('#drinks').show();
+  } else {
+    $('#under-21').show();
   }
 });
+
+    prompt(result);
+});
+
+
+
+
+        // <div id="pairingResult">
+        //   <h2>The Result of you answers yields the following Celebrity Pairing</h2>
+        //   <select class="form-control">
+        //     <option a>Make a selection</option>
+        //     <option b>Androgynous Andy</option>
+        //     <option c>Scintillating Sam</option>
+        //     <option d>Lucious Lex</option>
+        //   </select>
